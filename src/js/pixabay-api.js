810a606bf9aea1eddaa617a803083602497048cf
Sofia <sofiatselenx@gmail.com>
2024-03-10 00:07:24 +0200

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,8 +1,8 @@
-//const API_KEY='41849458-2d98265c06659a45ba73a30c';
+//const API_KEY='42660522-ae3104db13d278f91a0482c47';
 
 import axios from 'axios';
 
-const apiKey = '41849458-2d98265c06659a45ba73a30c';
+const apiKey = '42660522-ae3104db13d278f91a0482c47';
 export async function searchImages(query, page = 1, perPage = 15) {
     try {
         const response = await axios.get(`https://pixabay.com/api/?key=${apiKey}&q=${query}&page=${page}&per_page=${perPage}`);
